64bff118118323fbd47f58b702420e875e86f13b
Zihan Mei <meizihanyx@gmail.com>
2023-11-04 22:14:07 +0800

    header highlight modify

--- assets/js/headerhighlightdect.js
+++ assets/js/headerhighlightdect.js
@@ -1,10 +1,13 @@
 $().ready(function() {
     var ads = window.location.pathname;
     var pagename = ads.substr(1).split('.')[0];
+    // if (pagename == '' || pagename == 'index') {
+    //     $('#indexnav').addClass('active');
+    // }
     if (pagename == 'research') {
         $('#researchnav').addClass('active');
     }
-    else if (pagename == 'blog') {
+    else if (pagename == 'publications') {
         $('#publicationsnav').addClass('active');
     }
     // else if (pagename == 'tabs') {
